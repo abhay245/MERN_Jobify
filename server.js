9d30830f4327faee5b0fcc9017b4f9732ced502d
Abhay Kumar Singh <abhayanuj22@gmail.com>
2023-07-10 19:25:40 +0530

    mern_jobify

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const {dirname}=require('path')
-const {fileURLToPath}=require('url')
-const {path}=require('path')
+const { dirname } = require('path');
+const { fileURLToPath } = require('url');
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
 const connectDB = require('./db/connect.js');
 const authRouter = require('./routes/authRoutes.js');
 const jobsRouter = require('./routes/jobsRouter.js');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const authenticateUser = require('./middleware/auth.js')
-const morgan = require('morgan')
-const {helmet}=require('helment')
-const {xss} =require('xss-clean')
-const {mongoSanitize}=require('express-mongo-sanitize')
+const authenticateUser = require('./middleware/auth.js');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { xss } = require('xss-clean');
+const { mongoSanitize } = require('express-mongo-sanitize');
+
 // Middleware
-app.use(cors())
-// Setting up Morgan 
-if(process.env.NODE_ENV !='production'){
-  app.use(morgan('dev'))
+app.use(cors());
+
+// Setting up Morgan
+if (process.env.NODE_ENV != 'production') {
+  app.use(morgan('dev'));
 }
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
@@ -42,6 +44,7 @@ app.use('/jobs', authenticateUser, jobsRouter); // Handle routes starting with "
 app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
+
 // Handle 404 errors (Not Found)
 app.use(notFoundMiddleware);
 
@@ -50,6 +53,7 @@ app.use(notFoundMiddleware);
 
 // Define the port to listen on
 const port = process.env.PORT || 3000;
+
 // Start the server
 const start = async () => {
   await connectDB(process.env.MONGO_URL);
